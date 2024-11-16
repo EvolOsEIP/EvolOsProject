27ab52dbd12d81fed3b546f999651ec343e6cf84
nourrysebastieN <sebastien.nourr@epitech.eu>
2024-11-16 15:43:34 +0700

    [UPDATE] Back merged with front for mail sender

--- mailpage/lib/new_mail.dart
+++ mailpage/lib/new_mail.dart
@@ -138,6 +138,7 @@ class _NewMailState extends State<NewMail> {
             ),
             const SizedBox(height: 10),
 
+            // Zone de rédaction et barre d'outils
             Expanded(
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
