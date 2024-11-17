d5770eca10886fc75d5b9d52ea5b5aeeaee0c2c6
Clément Lagier <clement.lagier@epitech.eu>
2024-11-17 17:04:30 +0400

    [UPDATE] Made the subjects of the emails from the inbox page bolder
    Avoid pushing the pubspec.lock file

--- mailpage/lib/inbox.dart
+++ mailpage/lib/inbox.dart
@@ -78,7 +78,11 @@ class _InboxState extends State<Inbox> {
                   padding: const EdgeInsets.only(
                       left: 16.0, right: 16.0), // Add left and right margin
                   child: ListTile(
-                    title: Text(emailSubjects[index]),
+                    title: Text(
+                      emailSubjects[index],
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold), // Make the subject bold
+                    ),
                     subtitle: Text(emailPreviews[index]),
                   ),
                 );
